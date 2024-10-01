d56464ddac35883807362c8893cddb42ca78d71c
Dakodah Spurlin <dcspurlin@gmail.com>
2024-10-01 13:27:23 -0500

    Added files to repo

---
+++ variable.js
@@ -0,0 +1,22 @@
+console.log('JS');
+// declare and initalize variable
+let cars = 10;
+let bikes = 0;
+let vehicles = cars + bikes;
+console.log('Number of cars on hand', cars);
+console.log('Bikes in possesion', bikes);
+console.log('Number of vehicles available', vehicles);
+
+// decrement cars due to storing one
+cars--;
+console.log('Had to put a car into storage', cars);
+console.log('Bikes in possesion', bikes);
+console.log('Number of vehicles available', vehicles);
+
+//crement bike
+
+bikes+= 1;
+console.log('Missed stored car, so i bought a new bike', bikes);
+console.log('Number of vehicles available', vehicles);
+
+
